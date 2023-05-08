369c27f4d916193992a6cd498bab6d3973ecca42
Dmtr1i <dmitry1klimenko@mail.ru>
2023-05-08 18:29:59 +0300

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Каталог';
+?>
+<div class="site-index">
+
+    <div class="filter">
+    </div>
+
+    <div class="products">
+    </div>    
+    
+</div>
